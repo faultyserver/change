f8eb41029663570257c98525523f5cc7479e9787
Jon <jonegeland@gmail.com>
2019-08-19 23:37:26 -0700

    Updated README and example

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 An early, in-progress of Changesets in Crystal. Inspired by Elixir's [Ecto Changesets](https://hexdocs.pm/ecto/Ecto.Changeset.html).
 
+What works:
+
+  - `cast` with typecasting of primitive types (nil, booleans, string, ints, floats).
+  - `validate_*` for making assertions about fields (currently only `validate_required`).
+    - `valid?` method on the changeset to indicate overall validity.
+    - `add_error` to add an error to the changeset and mark it as invalid.
+  - `get_*` to inspect information about the changeset dynamically.
+  - Schema definitions (but this is definitely going to change a lot soon).
+
 Initial goals for this implementation include:
 
   - **Compile Performance:** minimal, linear/constant-time overhead with macros and generated code with respect to the number of usages.
--- example.cr
+++ example.cr
@@ -1,30 +1,54 @@
 require "./src/change"
 
 class User
-  Change::Changeset.schema User,
+  Change.schema User,
     name : String,
-    age : Int32
-
-  include Change::Changeset
+    age : Int32,
+    bio : String
 
   # Standard pattern for working with changesets. Define a static method
   # that performs casts and validations to abstract that from the callsite.
-  def self.changeset(instance, changes)
+  def self.changeset(instance, changes={} of String => String)
     Changeset.new(instance)
-      .cast(changes, [:name, :age])
+      .cast(changes, [:name, :age, :bio])
+      .validate_required(["name", "age"])
   end
 end
 
-# Both changes are valid and stored on the changeset
-changeset = User.changeset(User.new, {name: "Jon", age: 23})
+## Basic client usage
+
+# All changes are valid and stored on the changeset
+changeset =
+  User.changeset(User.new, {
+    name: "Jon",
+    age: 23,
+    bio: "I like changesets"
+  })
 pp changeset #=> @valid = true
+
+
+## Dealing with invalid props
+
 # Bool can't be casted to Int32, so the change is invalid and not stored,
-# and the changeset is marked as invalid.
-changeset = changeset.cast({age: true}, [:age])
-pp changeset #=> @valid = false
+# and the changeset is marked as invalid. `name `is also not given
+changeset = User.changeset(User.new, {age: true})
+# Even though `age` was given as a param, it is not valid and fails to `cast`,
+# so it keeps the initial value of `nil` and fails `validate_required`.
+pp changeset.errors
+
+
+## Using existing information
+
+user = User.new
+user.name = "John"
+user.age = 23
+# Initiailizing a changeset with an existing User lets it use that data
+# for validations.
+changeset = User.changeset(user)
+pp changeset #=> valid = true
 
 
 user = User.new
 pp user #=> Empty user
-changeset.apply(user)
+changeset.apply_changes(user)
 pp user #=> User with casted changes
