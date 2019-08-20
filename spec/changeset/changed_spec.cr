d1a131d9b845c18ea5e1cfe3dcf9778dd1910bcb
Jon <jonegeland@gmail.com>
2019-08-19 20:13:16 -0700

    add specs for `changed?`

---
+++ spec/changeset/changed_spec.cr
@@ -0,0 +1,33 @@
+require "../spec_helper"
+
+private class User
+  include AutoInitialize
+
+  Change::Changeset.schema User,
+    name : String,
+    age : Int32
+
+  def self.changeset(instance=self.new)
+    User::Changeset.new(instance)
+  end
+end
+
+describe Change::Changeset do
+  describe "#changed?" do
+    it "returns true if the changeset has any changes" do
+      changeset = User.changeset()
+        .cast({name: "John", age: 35}, [:name, :age])
+
+      changeset.changed?.should eq(true)
+    end
+
+    it "returns false if the changeset has no changes" do
+      user = User.new(name: "John", age: 35)
+
+      changeset = User.changeset(user)
+        .cast({name: "John", age: 35}, [:name, :age])
+
+      changeset.changed?.should eq(false)
+    end
+  end
+end
