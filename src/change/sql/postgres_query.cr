998ab56f2c8586f74bdcbbc474b9654fb5fe269f
Jon <jonegeland@gmail.com>
2019-08-26 21:49:40 -0700

    use class methods on PostgresQuery to make per-instance binds more natural

--- src/change/sql/postgres_query.cr
+++ src/change/sql/postgres_query.cr
@@ -2,8 +2,32 @@ module Change
   module SQL
     class PostgresQuery
       property query : Query
+      property binds : Array(DB::Any)
+
+
+      def self.select(query : Query)
+        q = self.new(query)
+        {q.to_select, q.binds}
+      end
+
+      def self.insert(query : Query)
+        q = self.new(query)
+        {q.to_insert, q.binds}
+      end
+
+      def self.update(query : Query)
+        q = self.new(query)
+        {q.to_update, q.binds}
+      end
+
+      def self.delete(query : Query)
+        q = self.new(query)
+        {q.to_delete, q.binds}
+      end
+
 
       def initialize(@query : Query)
+        @binds = [] of DB::Any
       end
 
       def to_select
@@ -25,6 +49,15 @@ module Change
       end
 
       def to_insert
+        String.build do |str|
+          str << " INSERT INTO "
+          build_from_expressions(str)
+          str << " ("
+          build_insert_field_list_expression(str)
+          str << ") VALUES ("
+          build_insert_value_list_expression(str)
+          str << ")"
+        end
       end
 
       def to_update
@@ -81,16 +114,18 @@ module Change
       private def build_where_expressions(io : IO)
         wheres = query.wheres.map do |expr|
           criterion = make_criterion(expr.criterion)
-          "#{criterion} #{expr.op} #{expr.value}"
+          bind = make_bind(expr.value)
+          "#{criterion} #{expr.op} #{bind}"
         end
 
-        io << wheres.join(", ")
+        io << wheres.join(" AND ")
       end
 
       private def build_update_expressions(io : IO)
         updates = query.updates.map do |expr|
           criterion = make_criterion(expr.criterion)
-          "#{criterion} = #{expr.value}"
+          bind = make_bind(expr.value)
+          "#{criterion} = #{bind}"
         end
 
         io << updates.join(", ")
@@ -106,6 +141,31 @@ module Change
         io << orders.join(", ")
       end
 
+      # INSERT uses the `criterion` fields of the `UpdateExpr` list on the
+      # query to build the list of fields that will be inserted.
+      private def build_insert_field_list_expression(io : IO)
+        fields = query.updates.map do |expr|
+          # Assuming that it's a single table, just the name of the column is
+          # needed.
+          expr.criterion.column
+        end
+
+        io << fields.join(", ")
+      end
+
+      # INSERT uses the `value` fields of the `UpdateExpr` list on the
+      # query to build the list of values that will be inserted.
+      #
+      # Because this is a list, the order should be guaranteed to match what
+      # was generated for the field list.
+      private def build_insert_value_list_expression(io : IO)
+        fields = query.updates.map do |expr|
+          make_bind(expr.value)
+        end
+
+        io << fields.join(", ")
+      end
+
 
       private def make_criterion(expr : SelectExpr)
         if table = expr.table
@@ -113,6 +173,13 @@ module Change
         else
           "#{expr.column}"
         end
+      end
+
+      # Adds a value to the list of binds for the query and returns the
+      # positional parameter to use in the query string to represent it.
+      private def make_bind(value : DB::Any)
+        @binds.push(value)
+        "$#{@binds.size}"
       end
     end
   end
